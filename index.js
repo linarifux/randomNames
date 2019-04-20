f80a16412e96710bcce07d6c5adfbf086b4baa56
linarifux <linarifux@gmail.com>
2019-04-21 00:35:51 +0600

    some new changes

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ var action = document.querySelector('#action');
 var inputForm = document.querySelector('#inputform');
 var result = document.querySelector('#result');
 
-var newName = ['Abul','Mofiz','Kangal','Damra','Pocha','Demon','Fokir','Gorib','Modon','Cute','Devil','Savage','Nowakhailla','Beyadob','Dreamer'];
-var randomNumber = parseInt(Math.random() * newName.length);
-console.log(newName[randomNumber]);
+
 
 function boom() {
   action.addEventListener('click',(boom)=>{
+    var newName = ['Abul','Mofiz','Kangal','Damra','Pocha','Demon','Fokir','Gorib','Modon','Cute','Devil','Savage','Nowakhailla','Beyadob','Dreamer'];
+    var randomNumber = parseInt(Math.random() * newName.length);
+    console.log(newName[randomNumber]);
     result.innerHTML = uname.value + ", you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
     uname.value = ""
   });

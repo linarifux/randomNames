2cb08a60129adb1434a104ecff150c05cca138f3
linarifux <linarifux@gmail.com>
2019-04-20 23:58:33 +0600

    version no 1.0.1

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ var action = document.querySelector('#action');
 var inputForm = document.querySelector('#inputform');
 var result = document.querySelector('#result');
 
+var newName = ['Abul','Mofiz','Kangal','Damra','Pocha','Demon','Fokir','Gorib','Modon'];
+var randomNumber = parseInt(Math.random() * newName.length);
+console.log(newName[randomNumber]);
+
 function boom() {
   action.addEventListener('click',(boom)=>{
-    result.innerHTML = uname.value;
+    result.innerHTML = uname.value + ", you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
+    uname.value = ""
   });
 };
 

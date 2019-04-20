7beb8cb65be28fcb0ca6980c38ab9d783ed2a4f9
linarifux <linarifux@gmail.com>
2019-04-21 00:38:35 +0600

    silly change

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ var result = document.querySelector('#result');
 
 
 
-function boom() {
+
   action.addEventListener('click',(boom)=>{
     var newName = ['Abul','Mofiz','Kangal','Damra','Pocha','Demon','Fokir','Gorib','Modon','Cute','Devil','Savage','Nowakhailla','Beyadob','Dreamer'];
     var randomNumber = parseInt(Math.random() * newName.length);
     console.log(newName[randomNumber]);
     result.innerHTML = uname.value + ", you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
     uname.value = ""
+    randomNumber = 0;
   });
-};
-
-boom();

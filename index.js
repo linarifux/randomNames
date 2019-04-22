c2f9371fb30e696579cbbb02ea77fe527ac9ef59
linarifux <linarifux@gmail.com>
2019-04-22 19:08:05 +0600

    added a condition for empty inputs in inex.js file

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ var uname = document.querySelector('#uname');
 var action = document.querySelector('#action');
 var inputForm = document.querySelector('#inputform');
 var result = document.querySelector('#result');
-
-
-
-
-  action.addEventListener('click',(boom)=>{
+// Submit Button will work for the following code
+action.addEventListener('click',(boom)=>{
     var newName = ['Abul','Mofiz','Kangal','Damra','Pocha','Demon','Fokir','Gorib','Modon','Cute','Devil','Savage','Nowakhailla','Beyadob','Dreamer'];
     var randomNumber = parseInt(Math.random() * newName.length);
     console.log(newName[randomNumber]);
-    result.innerHTML = uname.value + ", you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
+    if (uname.value === "") {
+      window.alert('Come on, Let us know your name')
+    }else {
+      result.innerHTML = "<b>" + uname.value + "</b>" + " you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
+    }
     uname.value = ""
-    randomNumber = 0;
+
   });

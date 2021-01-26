2db20bbcae4f67900128f71d2eae0852233eb2bd
Naimul Islam <linarifux@gmail.com>
2021-01-27 02:05:24 +0600

    Added keyup function to the app

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var uname = document.querySelector('#uname');
-var action = document.querySelector('#action');
-var inputForm = document.querySelector('#inputform');
-var result = document.querySelector('#result');
-// Submit Button will work for the following code
-action.addEventListener('click',(boom)=>{
-    var newName = ['fokir','Gorib','Abul','Mofiz','Damra','Hero Alom','Smart','GuluGulu','Awesome','Adorable','Angelic','Charming','Classy','Dazzling','Delightful','Cute','Gorgeous','Marvelous','Pretty','Stunning','Dreamer','Tantalizing','wonderful','Devil','Villain','Hero','Hitlar','Dreamer','Hablu','Lazy','Badass'];
-    var randomNumber = parseInt(Math.random() * newName.length);
-    console.log(newName[randomNumber]);
+var uname = document.querySelector('#uname')
+var action = document.querySelector('#action')
+var inputForm = document.querySelector('#inputform')
+var result = document.querySelector('#result')
+
+function getName(){
+    var newName = ['fokir','Gorib','Abul','Mofiz','Damra','Hero Alom','Smart','GuluGulu','Awesome','Adorable','Angelic','Charming','Classy','Dazzling','Delightful','Cute','Gorgeous','Marvelous','Pretty','Stunning','Dreamer','Tantalizing','wonderful','Devil','Villain','Hero','Hitlar','Dreamer','Hablu','Lazy','Badass']
+
+    var randomNumber = parseInt(Math.random() * newName.length)
+    // console.log(newName[randomNumber])
     if (uname.value === "") {
       window.alert('Come on, Let us know your name')
     }else {
-      result.innerHTML = "<b>" + uname.value + "</b>" + " you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside";
+      result.innerHTML = "<b>" + uname.value + "</b>" + " you are " + "<b>"+ newName[randomNumber] + "</b>" + " inside"
     }
     uname.value = ""
-
-  });
+}
+// Submit Button will work for the following code
+action.addEventListener('click', getName)
+uname.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13){
+    getName()
+  }
+})
